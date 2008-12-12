431c0e2931b561e667e25313b947a977801d03b7
Ondrej Certik <ondrej@certik.cz>
2008-12-12 16:04:17 +0100

    report.py: all in one plot

--- report.py
+++ report.py
@@ -1,23 +1,27 @@
 #! /usr/bin/env python
+print "loading"
 
 from tables import openFile
 from pylab import plot, show, legend, grid, xlabel, ylabel, yscale
 
 print "plotting"
 h5file = openFile("report.h5")
-table = h5file.root.schroed.sim
-#for iter in table:
-#    print "n:", iter["n"]
-#    print "cpu_solve:", iter["cpu_solve"]
-#    print "cpu_solve_reference:", iter["cpu_solve_reference"]
-#    print "DOF:", iter["DOF"], iter["DOF_reference"]
-#    print "errors:", iter["eig_errors"]
+table = h5file.root.schroed.single
 x = table.col("DOF")
 eig_errors = table.col("eig_errors")
 for i in range(len(eig_errors[0])):
     eig = [a[i] for a in eig_errors]
     plot(x, eig, linewidth=2, label="eigenvector %d" % i)
     plot(x, eig, "kD")
+    print i
+
+for i in range(4):
+    table = getattr(h5file.root.schroed, "eig%d" % i)
+    x = table.col("DOF")
+    eig_errors = table.col("eig_errors")
+    eig = [a[i] for a in eig_errors]
+    plot(x, eig, linewidth=2, label="multimesh eigenvector %d" % i)
+    plot(x, eig, "rD")
     print i
 h5file.close()
 
