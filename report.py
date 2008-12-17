56d5d139847f0f32d1524ef6833f34574a6b22a2
Ondrej Certik <ondrej@certik.cz>
2008-12-17 21:35:13 +0100

    report.py fixed

--- report.py
+++ report.py
@@ -6,19 +6,12 @@ from pylab import plot, show, legend, grid, xlabel, ylabel, yscale
 
 print "plotting"
 h5file = openFile("report.h5")
-table = h5file.root.schroed.single
+table = h5file.root.sim.iterations
 x = table.col("DOF")
 y = table.col("cpu_solve")
 plot(x, y, linewidth=2, label="single")
 plot(x, y, "kD")
 
-for i in range(4):
-    table = getattr(h5file.root.schroed, "eig%d" % i)
-    x = table.col("DOF")
-    y = table.col("cpu_solve")
-    plot(x, y, linewidth=2, label="multimesh eigenvector %d" % i)
-    plot(x, y, "rD")
-    print i
 h5file.close()
 
 xlabel("DOF")
