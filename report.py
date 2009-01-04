505384f94455c740174354ce9559d20d7c63104c
Ondrej Certik <ondrej@certik.cz>
2009-01-04 15:12:16 -0800

    plot everything

--- report.py
+++ report.py
@@ -11,19 +11,44 @@ if len(sys.argv) == 2:
     sim_name = sys.argv[1]
 else:
     sim_name = "sim"
-h5file = openFile("report.h5")
+h5file = openFile("report-h.h5")
 table = getattr(h5file.root, sim_name).iterations
 x = table.col("DOF")
 y = table.col("total_error")
-plot(x, y, "b-", linewidth=2, label="h-FEM total error")
+plot(x, y, "b-", linewidth=2, label="h-FEM (linear) total error")
 plot(x, y, "bD")
 h5file.close()
-h5file = openFile("report-august-hp.h5")
+
+h5file = openFile("report-h2.h5")
+table = getattr(h5file.root, sim_name).iterations
+x = table.col("DOF")
+y = table.col("total_error")
+plot(x, y, "m-", linewidth=2, label="h-FEM (quadratic) total error")
+plot(x, y, "mD")
+h5file.close()
+
+h5file = openFile("report-h2-b.h5")
+table = getattr(h5file.root, sim_name).iterations
+x = table.col("DOF")
+y = table.col("total_error")
+plot(x, y, "y-", linewidth=2, label="h-FEM (quadratic, thr=0.7) total error")
+plot(x, y, "yD")
+h5file.close()
+
+h5file = openFile("report-h2-c.h5")
+table = getattr(h5file.root, sim_name).iterations
+x = table.col("DOF")
+y = table.col("total_error")
+plot(x, y, "c-", linewidth=2, label="h-FEM (quadratic, thr=0.1) total error")
+plot(x, y, "cD")
+h5file.close()
+
+h5file = openFile("report-hp.h5")
 table = getattr(h5file.root, sim_name).iterations
 x = table.col("DOF")
 y = table.col("total_error")
 plot(x, y, "g-", linewidth=2, label="hp-FEM total error")
-#plot(x, y, "gD")
+plot(x, y, "gD")
 h5file.close()
 
 xlabel("DOF")
